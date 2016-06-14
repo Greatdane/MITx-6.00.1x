6b72998c871f2f4f8d55c58edc33fb44c2586049
David <gr3atdane@gmail.com>
2016-06-14 18:48:25 +0100

    Delete Lec4.4_problem.py

--- Lec4.4_problem.py
+++
@@ -1,8 +0,0 @@
-def evalQuadratic(a, b, c, x):
-    '''
-    a, b, c: numerical values for the coefficients of a quadratic equation
-    x: numerical value at which to evaluate the quadratic.
-    '''
-    ans = a * x**2 + b * x + c 
-    return ans
-    # can just use one line return a * x**2 + b * x + c
